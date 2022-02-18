db5233be550f2d1944046ada765fc4081e5af399
alexis roman <elsanguinario3257@gmail.com>
2022-02-18 09:57:18 -0600

    segunda clase

--- practica2/ViewController.swift
+++ practica2/ViewController.swift
@@ -2,7 +2,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var nombrenviar:String?
     @IBOutlet weak var nombreTF: UITextField!
     @IBOutlet weak var edadTF: UITextField!
     override func viewDidLoad() {
@@ -13,5 +13,14 @@ class ViewController: UIViewController {
     @IBAction func enviarBtn(_ sender: UIButton) {
         //print("enviar")
         performSegue(withIdentifier: "enviarParametros", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "enviarParametros"{
+            let objDestino = segue.destination as! pantalla2ViewController
+            objDestino.recibirNombre = nombreTF.text
+            objDestino.recibirEdad = edadTF.text
+            
+        }
     }
 }
--- practica2/pantalla2ViewController.swift
+++ practica2/pantalla2ViewController.swift
@@ -1,29 +1,39 @@
-//
-//  pantalla2ViewController.swift
-//  practica2
-//
-//  Created by Mac12 on 16/02/22.
-//
 
 import UIKit
 
 class pantalla2ViewController: UIViewController {
-
+    var recibirNombre: String?//especificamos variabeles opcional
+    var recibirEdad:String?//recibe edad
+    @IBOutlet weak var nombreusuario: UILabel!
+    @IBOutlet weak var imagenValidacion: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //modifica label
+        nombreusuario.text = recibirNombre
+        let conversion = Int(recibirEdad!) ?? 0
+        //if let edadInt = Int(recibirEdad!){
+         //   print("esta es una variable segura, iflet:")
+        //}
+        
+        if conversion > 18{
+            print("mayor de edad")
+            self.view.backgroundColor = UIColor.cyan
+            imagenValidacion.image = UIImage(named: "mayor")
+            nombreusuario.text = "Bienvenido \(recibirNombre) eres mayor de 18"
+            //systemName
+            
+        }else{
+            print("menor de edad")
+            self.view.backgroundColor = UIColor.red
+            imagenValidacion.image = UIImage(named: "U18")
+            "Bienvenido \(recibirNombre) eres menor de 18"
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func regresarBTn(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    @IBAction func salirBtn(_ sender: UIButton) {
+        exit(0)
+    }
 }
